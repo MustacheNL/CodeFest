9202bcccee7c6adb89a5af697a95f2750eee0ec7
Nyma Dolatkhahnejad <MustacheDeliva@gmail.com>
2017-04-12 13:59:18 +0200

    PDO connection

---
+++ includes/config.inc.php
@@ -0,0 +1,21 @@
+<?php
+if(stristr($_SERVER['REQUEST_URI'], 'config.inc.php')){
+    die("Wait a minute... who are you? You're not allowed to come here.");
+}
+
+/* Database Credentials */
+$DatabaseHost = "localhost"; //Database host (use localhost or 127.0.0.1)
+$DatabaseUser = "root"; //Database username login (standard root)
+$DatabasePassword = "codefest2017"; //Database password
+$DatabaseName = "Codefest"; //The database name (use "test" for test proposal, because test is always in a standard MySQL Database)
+
+/* Database Connection (PDO) */
+try {
+    $DatabaseConnection = new PDO("mysql:host=$DatabaseHost;dbname=$DatabaseName", $DatabaseUser, $DatabasePassword);
+    $DatabaseConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connectie succesvol gemaakt!"; //For test proposals, remove comment to see if connection is succesfull
+} catch(PDOexception $error) {
+    echo "Connectie mislukt zie foutmelding: " . $error->getMessage();
+    die();
+}
+?>
