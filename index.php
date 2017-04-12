67fa0e3cf0ca788bd1b3b80a9a4bdb92e2403ce2
Nyma Dolatkhahnejad <MustacheDeliva@gmail.com>
2017-04-12 23:56:14 +0200

    Changed login, changed register, cleaned code. Fixed presets, biggest commit ever.

--- index.php
+++ index.php
@@ -5,27 +5,27 @@ session_start();
 require_once("class.user.php");
 $login = new USER();
 
-if($login->is_loggedin()!="") {
+if($login->is_loggedin()!="")
+{
     $login->redirect('home.php');
 }
 
-if(isset($_POST['btn-login'])) {
+if(isset($_POST['btn-login']))
+{
     $uname = strip_tags($_POST['txt_uname_email']);
     $umail = strip_tags($_POST['txt_uname_email']);
     $upass = strip_tags($_POST['txt_password']);
 
-    if($login->doLogin($uname,$umail,$upass)) {
+    if($login->doLogin($uname,$umail,$upass))
+    {
         $login->redirect('uIndex.php');
-    } elseif($uname == "") {
-        $error = "Provide a username or email!";
-    } elseif(!$uname == "" && $upass == "") {
-        $error = "Provide a password!";
-    } else {
-        $error = "Wrong credentials!";
+    }
+    else
+    {
+        $error = "Wrong Details !";
     }
 }
 ?>
-
 <html lang="en">
 <body class="mdl-demo mdl-color--grey-100 mdl-color-text--grey-700 mdl-base">
 <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -39,45 +39,76 @@ if(isset($_POST['btn-login'])) {
         </div>
     </header>
     <main class="mdl-layout__content">
+
         <div class="demo-card-wide mdl-card mdl-shadow--2dp">
             <div class="container">
                 <div class="signin-form">
+
                     <div class="container">
-                        <h2 class="form-signin-heading">Log In</h2><hr />
-                        <div id="error">
-                            <?php
-                            if(isset($error)) {
-                            ?>
-                            <span class="mdl-chip mdl-chip--contact">
-                                <span class="mdl-chip__contact mdl-color--red mdl-color-text--white">!</span>
-                                <span class="mdl-chip__text"><?php echo $error; ?></span>
-                            </span>
-                            <?php } ?>
-                        </div>
-                        <form action="index.php" method="post">
-                            <div class="mdl-textfield mdl-js-textfield">
-                                <input class="mdl-textfield__input" type="text" id="sample1" name="txt_uname_email">
-                                <label class="mdl-textfield__label" for="sample1">Username or E-mail...</label>
-                            </div>
-                            <div class="mdl-textfield mdl-js-textfield">
-                                <input class="mdl-textfield__input" type="password" id="sample1" name="txt_password">
-                                <label class="mdl-textfield__label" for="sample1">Your password...</label>
+
+
+                            <h2 class="form-signin-heading">Log In</h2><hr />
+
+                            <div id="error">
+                                <?php
+                                if(isset($error))
+                                {
+                                    ?>
+                                    <div class="alert alert-danger">
+                                        <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
+                                    </div>
+                                    <?php
+                                }
+                                ?>
                             </div>
 
-                            <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored, btn btn-default" type="submit" name="btn-login">
-                                <i class="glyphicon glyphicon-log-in"></i> Sign in
-                            </button>
-                        </form>
-                        <br>
-                        <br />
-                        <label>Don't have an account yet? <a href="register.php">Sign up</a>!</label>
+                            <form action="index.php" method="post">
+                                <div class="mdl-textfield mdl-js-textfield">
+                                    <input class="mdl-textfield__input" type="text" id="sample1" name="txt_uname_email">
+                                    <label class="mdl-textfield__label" for="sample1">Username or E-mail ID</label>
+                                </div>
+                                <div class="mdl-textfield mdl-js-textfield">
+                                <input class="mdl-textfield__input" type="password" id="sample1" name="txt_password">
+                                <label class="mdl-textfield__label" for="sample1">Your Password</label>
+                                </div>
+
+                                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored, btn btn-default" type="submit" name="btn-login">
+                                    <i class="glyphicon glyphicon-log-in"></i> Sign In
+                                </button>
+                            </form>
+
+<!--                            <div class="form-group">-->
+<!--                                <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or E mail ID" required />-->
+<!--                                <span id="check-e"></span>-->
+<!--                            </div>-->
+<!---->
+<!--                            <div class="form-group">-->
+<!--                                <input type="password" class="form-control" name="txt_password" placeholder="Your Password" />-->
+<!--                            </div>-->
+<!---->
+<!--                            <hr />-->
+
+
+<!--                            <div class="form-group">-->
+<!--                                <button type="submit" name="btn-login" class="btn btn-default">-->
+<!--                                    <i class="glyphicon glyphicon-log-in"></i> &nbsp; SIGN IN-->
+<!--                                </button>-->
+<!--                            </div>-->
+                            <br>
+                            <br />
+                            <label>Don't have account yet! <a href="register.php">Sign Up</a></label>
+
+
+
+
                     </div>
+
                 </div>
             </div>
         </div>
         <?php include 'includes/footer.inc.php' ?>
     </main>
 
-    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+<script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 </body>
 </html>
