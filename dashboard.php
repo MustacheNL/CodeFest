2217b3e82fa992d73f08752ed151b32e7117982f
MrKytman <meesvanaarsen@gmail.com>
2017-04-13 01:22:12 +0200

    YES

--- dashboard.php
+++ dashboard.php
@@ -8,6 +8,56 @@ $user_id = $_SESSION['user_session'];
 $stmt = $auth_user->runQuery("SELECT * FROM users WHERE user_id=:user_id");
 $stmt->execute(array(":user_id"=>$user_id));
 $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
+
+if(isset($_POST['btn-signup'])) {
+    $uname = strip_tags($_POST['txt_uname']);
+    $umail = strip_tags($_POST['txt_umail']);
+    $upass = strip_tags($_POST['txt_upass']);
+    $ustreet = strip_tags($_POST['txt_street']);
+    $uzipcode = strip_tags($_POST['txt_zipcode']);
+    $ucity = strip_tags($_POST['txt_city']);
+    $ucountry = strip_tags($_POST['txt_country']);
+
+    if ($uname == "") {
+        $error[] = "Provide a username!";
+    } else if ($umail == "") {
+        $error[] = "Provide a e-mail!";
+    } else if (!filter_var($umail, FILTER_VALIDATE_EMAIL)) {
+        $error[] = 'Provide a valid e-mail!';
+    } else if ($upass == "") {
+        $error[] = "Provide a password!";
+    } else if (strlen($upass) < 6) {
+        $error[] = "Password must be atleast 6 characters!";
+    } else if ($_POST['txt_upass'] != $_POST['txt_upass2']) {
+        $error[] = "The entered passwords are not the same!";
+    } else if ($ustreet == "") {
+        $error[] = "Provide a street name!";
+    } else if ($uzipcode == "") {
+        $error[] = "Provide a ZIP code!";
+    } else if ($ucity == "") {
+        $error[] = "Provide a city name!";
+    } else if ($ucountry == "") {
+        $error[] = "Provide a country name!";
+    } else {
+        try {
+            $stmt = $user->runQuery("SELECT user_name, user_email FROM users WHERE user_name=:uname OR user_email=:umail");
+            $stmt->execute(array(':uname' => $uname, ':umail' => $umail));
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($row['user_name'] == $uname) {
+                $error[] = "Username already taken!";
+            } else if ($row['user_email'] == $umail) {
+                $error[] = "E-mail already taken!";
+            } else {
+                if ($user->register($uname, $umail, $upass, $ustreet, $uzipcode, $ucity, $ucountry)) {
+                    $user->redirect('register.php?joined');
+                }
+            }
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
+    }
+}
 ?>
 <head>
     <link rel="stylesheet" href="css/styles-dashboard.css">
@@ -27,6 +77,7 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script>
         $(document).ready(function(){
+            $(".contentdefault").show();
             $(".content1").hide();
             $(".content2").hide();
             $(".content3").hide();
@@ -35,16 +86,19 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
                 $(".content1").show();
                 $(".content2").hide();
                 $(".content3").hide();
+                $(".contentdefault").hide();
             });
             $("#content2").click(function(){
                 $(".content2").show();
                 $(".content1").hide();
                 $(".content3").hide();
+                $(".contentdefault").hide();
             });
             $("#content3").click(function(){
                 $(".content3").show();
                 $(".content2").hide();
                 $(".content1").hide();
+                $(".contentdefault").hide();
             });
         });
     </script>
@@ -68,14 +122,14 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
                     <span class="visuallyhidden">Accounts</span>
                 </button>
                 <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect" for="accbtn">
-                    <li class="mdl-menu__item" id="Register">Add account...</li>
+                    <li class="mdl-menu__item" id="content1">Add account...</li>
                     <li class="mdl-menu__item">Delete account...</li>
                     <a href="logout.php?logout=true" style="text-decoration: none;"><li class="mdl-menu__item">Log out...</li></a>
                 </ul>
             </div>
         </header>
         <nav class="demo-navigation mdl-navigation mdl-color--blue-grey-800">
-            <a class="mdl-navigation__link" id="content1" href="#">Betalingen huur</a>
+            <a class="mdl-navigation__link"  href="#">Betalingen huur</a>
             <a class="mdl-navigation__link" id="content2" href="#">Exporteren</a>
             <a class="mdl-navigation__link" id="content3" href="#">Statistieken</a>
             <div class="mdl-layout-spacer"></div>
@@ -83,8 +137,77 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
     </div>
     <main class="mdl-layout__content mdl-color--grey-100">
         <div class="demo-charts mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+            <div class="contentdefault " style="width: 100%">
+                <div class="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
+                    <div class="mdl-card__title mdl-card--expand mdl-color--teal-300">
+                        <h2 class="mdl-card__title-text">Updates</h2>
+                    </div>
+                    <div class="mdl-card__supporting-text mdl-color-text--grey-600">
+                        Non dolore elit adipisicing ea reprehenderit consectetur culpa.
+                    </div>
+                    <div class="mdl-card__actions mdl-card--border">
+                        <a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect">Read More</a>
+                    </div>
+                </div>
+            </div>
+
             <div class="content1" style="width: 100%">
-                <p>content1</p>
+                <form action="register.php" method="post" class="form-signin">
+                    <h2 class="form-signin-heading">Sign Up</h2><hr />
+                    <?php
+                    if(isset($error)) {
+                        foreach($error as $error) {
+                            ?>
+                            <span class="mdl-chip mdl-chip--contact">
+                                <span class="mdl-chip__contact mdl-color--red mdl-color-text--white">!</span>
+                                <span class="mdl-chip__text"><?php echo $error; ?></span>
+                            </span>
+                            <?php
+                        }
+                    } else if(isset($_GET['joined'])) {
+                        header( "refresh:5;url=index.php" );
+                        ?>
+                        <span class="mdl-chip mdl-chip--contact">
+                                <span class="mdl-chip__contact mdl-color--green mdl-color-text--white">:D</span>
+                                <span class="mdl-chip__text">Succesfully registered, you will be redirected to the home in 5 seconds. If not click <a href='index.php'>here</a>!</span>
+                            </span>
+                    <?php } ?>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_uname" value="<?php if(isset($error)){echo $uname;}?>" >
+                        <label class="mdl-textfield__label" for="sample1">Username...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_umail" value="<?php if(isset($error)){echo $umail;}?>" >
+                        <label class="mdl-textfield__label" for="sample1">E-mail...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password" id="sample1" name="txt_upass" >
+                        <label class="mdl-textfield__label" for="sample1">Password...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="password" id="sample1" name="txt_upass2" >
+                        <label class="mdl-textfield__label" for="sample1">Password again...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_street" >
+                        <label class="mdl-textfield__label" for="sample1">Street...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_zipcode" >
+                        <label class="mdl-textfield__label" for="sample1">Zipcode...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_city" >
+                        <label class="mdl-textfield__label" for="sample1">City...</label>
+                    </div>
+                    <div class="mdl-textfield mdl-js-textfield">
+                        <input class="mdl-textfield__input" type="text" id="sample1" name="txt_country" >
+                        <label class="mdl-textfield__label" for="sample1">Country...</label>
+                    </div>
+                    <button id="signup" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored,  btn-primary" type="submit" name="btn-signup">
+                        <i class="glyphicon glyphicon-open-file"></i> Sign up
+                    </button>
+                </form>
             </div>
 
             <div class="content2" style="width: 100%">
